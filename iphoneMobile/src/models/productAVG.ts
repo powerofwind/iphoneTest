7a69d7ed78cb6691916873a44c65ca4d3a5716ae
powerofwind <powerofwind10@gmail.com>
2018-12-31 13:54:39 +0700

    binding AVG (not complete)

---
+++ iphoneMobile/src/models/productAVG.ts
@@ -0,0 +1,6 @@
+import { product } from "./product";
+
+export class productAVG {
+   public allproduct: product[];
+   public average: number;
+}
--- iphoneMobile/src/pages/list/list.ts
+++ iphoneMobile/src/pages/list/list.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { HttpClient } from '@angular/common/http';
 import { product } from '../../models/product';
+import { productAVG } from '../../models/productAVG';
 
 @Component({
   selector: 'page-list',
@@ -9,17 +10,17 @@ import { product } from '../../models/product';
 })
 export class ListPage {
 
-  public products: product[];
+  public allproduct: any = [];
 
   constructor(public http: HttpClient, public navCtrl: NavController) {
-    
+
   }
 
-  ionViewWillEnter(){
-    this.http.get<product[]>("https://localhost:5001/api/Product").subscribe(
+  ionViewWillEnter() {
+    this.http.get<productAVG>("https://localhost:5001/api/Product/GetAVG").subscribe(
       it => {
         console.log(it)
-        this.products = it;
+        this.allproduct = it;
       });
   }
 
